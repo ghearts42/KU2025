39a0eb4e42841700bb0a35581a86b67ed0186b06
KU_CLASS <ghearts4239@gmail.com>
2025-03-05 21:19:47 +0900

    비트 연산자 실습

--- c_example/foutBit.c
+++ c_example/foutBit.c
@@ -29,10 +29,10 @@ int main(void){
 
 void printAttrib(unsigned char attr){
     printf("attr %d\n", attr);
-    printf("BOLD : %d\n", !(~(attr | ~BOLD)));
-    printf("ITALIC : %d\n", !(~(attr | ~ITALIC)>>1));
-    printf("SHADOW : %d\n", !(~(attr | ~SHADOW)>>2));
-    printf("UNDERLINE : %d\n", !(~(attr | ~UNDERLINE)>>3));
+    printf("BOLD : %d\n", (attr & BOLD));
+    printf("ITALIC : %d\n", ((attr & ITALIC)>>1));
+    printf("SHADOW : %d\n", ((attr & SHADOW)>>2));
+    printf("UNDERLINE : %d\n", ((attr & UNDERLINE)>>3));
     printf("------------------------------------\n");
 
 }
