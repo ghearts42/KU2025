05e8d93c8cc2f1b1be57fdf7458736d1e05fbc39
KU_CLASS <ghearts4239@gmail.com>
2025-03-11 17:15:30 +0900

    250311 1714 문자열 함수 구현 실습(보충 필요)

---
+++ c_example/part3/string/main.c
@@ -0,0 +1,23 @@
+#include "Mystring.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "wonderful C class";
+    int len = my_strlen(str);
+    printf("len of str : %d", len);
+
+    char str2[40];
+    my_strcpy(str2, str);
+    printf("%s\n", str2);
+
+    if (my_strcmp(str2, str) == 0)
+        printf("두 문자열의 내용은 같다.");
+    else
+        printf("두 문자열의 내용은 다르다.");
+
+    my_strcat(str2, "----");
+    my_strcat(str2, str);
+    printf("%s\n", str2);
+    return 0;
+}
