77347568f727ea2bdb33d576d7b20237feaa7ff7
KU_CLASS <ghearts4239@gmail.com>
2025-03-12 12:26:04 +0900

    250312 1230 SQL 및 외부 라이브러리 호출 기초 예제

---
+++ c_example/sqlquery/bookSql.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <mysql.h>
+
+// sudo apt install libmysql++
+// dpkg -L libmysqlclient -dev | grep mysql.h
+// cc -o booksql bookSql.c -I/usr/include/mysql -L/usr/lib/mysql -lmysqlclient
+// 오브젝트 명 : libmysqlclient.so || libmysqlclient.a
+
+int main(void)
+{
+    return 0;
+}
